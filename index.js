586a6029ef97a7d9100b93957b1f69f2831348a7
DinaLeahG <65265907+DinaLeahG@users.noreply.github.com>
2020-07-02 17:08:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("header").style.height = "65.5px";
-   document.getElementById("logo").style.height = '140px';
-   document.getElementById("logo").style.width = '196px';
+   document.getElementById("logo").style.height = "140px";
+   document.getElementById("logo").style.width = "196px";
         document.getElementById("logo").style.marginTop= "-52px";
 
   } else {
     document.getElementById("header").style.height = "98.25px";
-       document.getElementById("logo").style.height = '200px';
-    document.getElementById("logo").style.width = '280px';
+       document.getElementById("logo").style.height = "200px";
+    document.getElementById("logo").style.width = "280px";
      document.getElementById("logo").style.marginTop= "-65px";
 
   }
